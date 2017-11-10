500842eb8e2a164ebe0ef5823d51e8639205f66a
vladimir.grishanin <vladimir.grishanin@akvelon.com>
2017-11-10 18:09:01 +0300

    Implement cards component.
    Implemented component and its controller.

--- LanguageCards.WebApp/ClientApp/app/app.module.shared.ts
+++ LanguageCards.WebApp/ClientApp/app/app.module.shared.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { CardsComponent } from './components/cards/cards.component'
 
 @NgModule({
     declarations: [
@@ -16,7 +17,8 @@ import { CounterComponent } from './components/counter/counter.component';
         NavMenuComponent,
         CounterComponent,
         FetchDataComponent,
-        HomeComponent
+        HomeComponent,
+        CardsComponent
     ],
     imports: [
         CommonModule,
@@ -27,6 +29,7 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'home', component: HomeComponent },
             { path: 'counter', component: CounterComponent },
             { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'cards', component: CardsComponent },
             { path: '**', redirectTo: 'home' }
         ])
     ]
---
+++ LanguageCards.WebApp/ClientApp/app/components/cards/cards.component.ts
@@ -0,0 +1,22 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Component({
+    selector: 'cards',
+    templateUrl: './cards.component.html'
+})
+export class CardsComponent {
+    public cards: Cards[];
+
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'api/Cards/GetCards').subscribe(result => {
+            this.cards = result.json() as Cards[];
+        }, error => console.error(error));
+    }
+}
+
+interface Cards {
+    word: string
+    definition: string
+    example: string
+}
