5b4c1d3979c63bb67a0cf780cac949edfb683bcd
vladimir.grishanin <vladimir.grishanin@akvelon.com>
2017-12-01 14:20:25 +0300

    Add a component of card
    Component of card added.

---
+++ LanguageCards.WebApp/ClientApp/app/components/card/card.component.ts
@@ -0,0 +1,88 @@
+﻿import { Component, Inject, Input } from '@angular/core';
+import { Http } from '@angular/http';
+import { Card } from '../../models/card';
+import { Word } from '../../models/word';
+
+@Component({
+    selector: 'card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css'],
+})
+
+export class CardComponent {
+    @Input() cardItem: Card = {
+        id: 1,
+        word: {
+            id: 1,
+            text: "creation",
+            definition: "The act of creating something, or the thing that is created",
+            example: "The creation of a new political party.",
+            languageId: 1,
+            language: {
+                id: 1,
+                name: "English (United States)",
+            },
+            speechPartId: 1,
+            speechPart: {
+                id: 1,
+                name: "Noun",
+            },
+            translations: [
+                {
+                    id: 13,
+                    text: "Творчество",
+                    definition: "",
+                    example: "",
+                    languageId: 2,
+                    language: {
+                        id: 2,
+                        name: "Russian",
+                    },
+                    speechPartId: 1,
+                    speechPart: {
+                        id: 1,
+                        name: "Noun",
+                    },
+                    translations: [],
+                },
+                {
+                    id: 15,
+                    text: "Созидание",
+                    definition: "",
+                    example: "",
+                    languageId: 2,
+                    language: {
+                        id: 2,
+                        name: "Russian",
+                    },
+                    speechPartId: 1,
+                    speechPart: {
+                        id: 1,
+                        name: "Noun",
+                    },
+                    translations: [],
+                },
+                {
+                    id: 19,
+                    text: "Создание",
+                    definition: "",
+                    example: "",
+                    languageId: 2,
+                    language: {
+                        id: 2,
+                        name: "Russian",
+                    },
+                    speechPartId: 1,
+                    speechPart: {
+                        id: 1,
+                        name: "Noun",
+                    },
+                    translations: [],
+                }
+            ],
+        },
+    };
+    public answer: string = "";
+
+    constructor() { }
+}
