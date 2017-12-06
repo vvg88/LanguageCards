0c52b3a0cc99d9c43e179b36aabf873f409f4a6f
vladimir.grishanin <vladimir.grishanin@akvelon.com>
2017-12-06 10:53:17 +0300

    Change the style of sign-in component
    The style of sign-in component changed. The stylesheet added.

--- LanguageCards.WebApp/ClientApp/app/components/signin/signin.component.ts
+++ LanguageCards.WebApp/ClientApp/app/components/signin/signin.component.ts
@@ -5,7 +5,8 @@ import { SignInCredentials } from '../../models/signInCredentials'
 
 @Component({
     selector: 'signin',
-    templateUrl: './signin.component.html'
+    templateUrl: './signin.component.html',
+    styleUrls: ['./signin.component.css'],
 })
 
 export class SigninComponent {
